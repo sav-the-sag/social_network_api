fcc8e3071661f20e2c95204c93195e7da48eff0f
savan <savannahwilliams243@gmail.com>
2024-02-19 09:01:40 -0800

    added middleware and app.listen to index.js

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// allows server to listen for requests and send responses
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
