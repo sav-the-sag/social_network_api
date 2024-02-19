b9db6c10817a9663b77ce25d2c63638d569f7ad4
savan <savannahwilliams243@gmail.com>
2024-02-19 08:59:40 -0800

    added imports to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// imports
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
