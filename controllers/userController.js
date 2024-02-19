387c26761a79bbbfb4f41fe7cfd996d49d2b3be0
savan <savannahwilliams243@gmail.com>
2024-02-19 09:25:26 -0800

    started to get all users with User.find

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
 // imports
 const { User, Thought } = require("../models");
 
+// get all users
+const userController = {
+    async getUsers(req, res) {
+      try {
+        const users = await User.find()
+  
+          .populate({ path: "thoughts", select: "-__v" })
+          .populate({ path: "friends", select: "-__v" });
+  
+        return res.status(200).json(users);
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+      }
+    },
+
+}
