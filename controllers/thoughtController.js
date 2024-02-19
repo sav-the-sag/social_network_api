f2bb8602029eb716127680e7dd659744118d676d
savan <savannahwilliams243@gmail.com>
2024-02-19 09:35:36 -0800

    grab a single thought with Thought.findOne

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,13 +4,27 @@ const { User, Thought } = require("../models");
 // Get all thoughts
 const thoughtController = {
     async getThoughts(req, res) {
-      try {
-        const thoughts = await Thought.find();
-        return res.status(200).json(thoughts);
-      } catch (err) {
-        console.log(err);
-        return res.status(500).json(err);
-      }
+        try {
+            const thoughts = await Thought.find();
+            return res.status(200).json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     },
-    
+    // get a single thought
+    async getThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: "No thought with that ID" });
+            }
+
+            return res.status(200).json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 };
