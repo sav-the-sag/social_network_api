e648361d12e51d4f6b7f65c087fa0d28d0a60707
savan <savannahwilliams243@gmail.com>
2024-02-19 09:49:56 -0800

    seeded data in seed.js

---
+++ utils/seed.js
@@ -0,0 +1,31 @@
+// imports
+const { User, Thought, Reaction } = require("../models");
+const mongoose = require("mongoose");
+
+const connection = require("../config/connection");
+
+// seed data
+const users = [
+  {
+    username: "Sav",
+    email: "sav@gmail.com",
+    thought: [],
+  },
+];
+
+console.log(connection);
+
+// connects to server
+connection.once("open", async () => {
+  console.log("connected");
+
+// drop existing students
+  await User.deleteMany({});
+
+// adds seed data to database
+  await User.collection.insertMany(users);
+
+  console.table(users);
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
