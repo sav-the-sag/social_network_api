7fc6139d73e20df5268537ea63d58346bf5f01e6
savan <savannahwilliams243@gmail.com>
2024-02-19 09:46:42 -0800

    added imports to userRoutes and defined routes, then exported router

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+// imports
+const router = require("express").Router();
+
+// imports functions written in userController.js file
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// Route -> http://localhost:3001/api/users
+router.route("/").get(getUsers).post(createUser);
+
+// Route -> http://localhost:3001/api/users/:userId
+router
+  .route("/:userId")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// Route -> http://localhost:3001/api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+// Exports
+module.exports = router;
