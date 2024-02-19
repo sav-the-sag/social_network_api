c385927f6cc1d8bbe94029aaf271d58c7c9f2446
savan <savannahwilliams243@gmail.com>
2024-02-19 09:34:44 -0800

    begin thoughtController by grabbing all thoughts with Thought.find()

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,2 +1,16 @@
 // imports
 const { User, Thought } = require("../models");
+
+// Get all thoughts
+const thoughtController = {
+    async getThoughts(req, res) {
+      try {
+        const thoughts = await Thought.find();
+        return res.status(200).json(thoughts);
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+      }
+    },
+    
+};
