f7c32c9d14eb6bae57ffc1401f8074f0f2c4e899
savan <savannahwilliams243@gmail.com>
2024-02-19 09:26:27 -0800

    get a single user with User.findOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,17 +4,34 @@ const { User, Thought } = require("../models");
 // get all users
 const userController = {
     async getUsers(req, res) {
-      try {
-        const users = await User.find()
-  
-          .populate({ path: "thoughts", select: "-__v" })
-          .populate({ path: "friends", select: "-__v" });
-  
-        return res.status(200).json(users);
-      } catch (err) {
-        console.log(err);
-        return res.status(500).json(err);
-      }
-    },
+        try {
+            const users = await User.find()
 
+                .populate({ path: "thoughts", select: "-__v" })
+                .populate({ path: "friends", select: "-__v" });
+
+            return res.status(200).json(users);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    // get a single user
+    async getUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+
+                .populate({ path: "thoughts", select: "-__v" })
+                .populate({ path: "friends", select: "-__v" });
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID" });
+            }
+
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 }
