e99ba67825876e3453bdd6eb366e0d5bf58d4599
savan <savannahwilliams243@gmail.com>
2024-02-19 09:37:04 -0800

    update a thought with Thought.findOneAndUpdate

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -44,4 +44,23 @@ const thoughtController = {
             return res.status(500).json(err);
         }
     },
+    // update thought
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+
+            return res.status(200).json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 };
