59b0ea0b8fd0dc54542c73d913629ce07f9e5ed8
savan <savannahwilliams243@gmail.com>
2024-02-19 09:17:16 -0800

    imported and created Thought schema

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+// imports
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+// Thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  });
