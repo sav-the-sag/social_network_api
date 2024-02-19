0209ac6b7cf56fe9bd74f956fbb364405769572a
savan <savannahwilliams243@gmail.com>
2024-02-19 09:29:31 -0800

    delete a user with User.findOneAndDelete

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,4 +63,22 @@ const userController = {
             return res.status(500).json(err);
         }
     },
+    // delete user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID" });
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            return res.status(200).json({
+                message: "User and associated thoughts and reactions deleted!",
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 }
