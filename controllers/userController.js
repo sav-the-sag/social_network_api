eeec07f0ce27cb0268e4ca3de3f48c493cc22beb
savan <savannahwilliams243@gmail.com>
2024-02-19 09:31:36 -0800

    delete a friend with User.findOneAndUpdate

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,4 +100,23 @@ const userController = {
             return res.status(500).json(err);
         }
     },
-}
+    // delete a friend
+    async deleteFriend(req, res) {
+        try {
+            const friend = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!friend) {
+                return res.status(404).json({ message: "Check user and friend ID" });
+            }
+
+            return res.status(200).json(friend);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+};
